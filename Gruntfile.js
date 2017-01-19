3f94e7961c0f7be3ad97b1695dbc6d8d6cb3dc4c
Sam Brown <sabrown@dare2b.com>
2017-01-19 16:14:31 +0000

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    // Watch task config
+    watch: {
+      sass: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    },
+    // SASS task config
+    sass: {
+        dev: {
+            files: {
+                // destination         // source file
+                "regatta/css/styles.css" : "regatta/scss/styles.scss"
+            }
+        }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+};
