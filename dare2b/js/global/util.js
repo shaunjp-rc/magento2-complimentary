32b0ea41a2ab0628e803f8f99352af72d74b263b
Richard Silcock <rsilcock@regatta.com>
2018-04-16 08:19:31 +0000

    jQuery untick box fix

--- dare2b/js/global/util.js
+++ dare2b/js/global/util.js
@@ -8,7 +8,7 @@
 
 /*///////// Unticking account sign up ////////*/
 requirejs(['jquery'], function( $ ) {
-    jQuery('.customer-account-create .newsletter .c-checkbox-option__label input').prop('checked', false);
+    jQuery('.customer-account-create .newsletter .c-checkbox-option__label input, .guest-checkout-login-account-create .newsletter .c-checkbox-option__label input').prop('checked', false);
 });
 
 /*///////// Badge & Promo Behaviour ////////*/
