80372b65661f86ab6986419c81fafe9af2bb4d2d
rimmerz <zrimmer@regatta.com>
2018-04-18 15:11:16 +0000

    tickbox removal

--- craghoppers/js/global/util.js
+++ craghoppers/js/global/util.js
@@ -4,11 +4,6 @@
 // Author: Shaun Pelling
 /////////////////////////////////////*/
 
-/*///////// Unticking account sign up ////////*/
-
-requirejs(['jquery'], function( $ ) {
-    jQuery('.customer-account-create .newsletter .c-checkbox-option__label input, .guest-checkout-login-account-create .newsletter .c-checkbox-option__label input').prop('checked', false);
-});
 
 /*///////// Badge & Promo Behaviour ////////*/
 
