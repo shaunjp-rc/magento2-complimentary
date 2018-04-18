8a47708d888be53ccaf6a7062d3882c6c23c27c9
Richard Silcock <rsilcock@regatta.com>
2018-04-18 15:16:30 +0000

    Removed JS

--- regatta/js/global/util.js
+++ regatta/js/global/util.js
@@ -5,9 +5,9 @@
 /////////////////////////////////////*/
 
 /*///////// Unticking account sign up ////////*/
-requirejs(['jquery'], function( $ ) {
-    jQuery('.customer-account-create .newsletter .c-checkbox-option__label input, .guest-checkout-login-account-create .newsletter .c-checkbox-option__label input').prop('checked', false);
-});
+// requirejs(['jquery'], function( $ ) {
+//     jQuery('.customer-account-create .newsletter .c-checkbox-option__label input, .guest-checkout-login-account-create .newsletter .c-checkbox-option__label input').prop('checked', false);
+// });
 
 /*///////// Badge & Promo Behaviour ////////*/
 
